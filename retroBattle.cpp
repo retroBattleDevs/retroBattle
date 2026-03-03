b681254a02f72212dede0d1ffdd6badafb5e94b0
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2026-03-04 00:10:33 +0100

    Created a non-Blocking game loop and user input dispatcher.

--- retroBattle.cpp
+++ retroBattle.cpp
@@ -1,41 +1,80 @@
-﻿#include <Windows.h>
+﻿#include <iostream>
+#include <Windows.h>
 #include "external_libraries/PDCurses/curses.h"
+#include <time.h>
+#include <cstdint>
 
-int main() {
-
+void drawBox(int &y, int &x) {
+	attron(COLOR_PAIR(1));
+	mvprintw(y - 2, x - 2, "#####");
+	mvprintw(y - 1, x - 2, "#   #");
+	mvprintw(y, x - 2, "#   #");
+	mvprintw(y + 1, x - 2, "#   #");
+	mvprintw(y + 2, x - 2, "#####");
+	attroff(COLOR_PAIR(1));
+	mvprintw(y, x, "#");
+}
+void keyDispatcher(const char c, int &y, int &x) {
+	switch (c) {
+		case 'a':
+			x--;
+			break;
+		case 'd':
+			x++;
+			break;
+		case 'w':
+			y--;
+			break;
+		case 's':
+			y++;
+			break;
+	}
+}
+void initWindowsAPI(void) {
 	HWND hwnd = GetConsoleWindow();
 	HWND owner = GetWindow(hwnd, GW_OWNER);
 	SendMessage(owner, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
 	Sleep(10);
-
+}
+void initPDCurses(void) {
 	initscr();
-
+	curs_set(0);
+	noecho();
+	nodelay(stdscr, true);
 	if (has_colors()) {
 		start_color();
 	}
+}
+int main() {
+	using namespace std;
 
-	int _rows = 0, _cols = 0;
+	initWindowsAPI();
+
+	initPDCurses();
+
+	int _rows = 0,
+		_cols = 0;
 	getmaxyx(stdscr, _rows, _cols);
+	int centerY = _rows / 2,
+		centerX = _cols / 2;
 
 	init_pair(1, COLOR_GREEN, COLOR_BLACK);
 
+	int x = 10, y = 10;
+
 	while (1) {
 
-		move(40, _cols - 12);  // Rows and Columns in terminal are starting from position Zero.
+		// Drawing of the Entities goes here.
+		drawBox(y, x);
+		mvprintw(0, 0, "y: %d    x: %d", y, x);
 
-		attron(COLOR_PAIR(1));
-		printw("Hello World!");
-		attroff(COLOR_PAIR(1));
+		int c = getch(stdin);
+		keyDispatcher(c, y, x);
 
-		refresh();
+		// Collision detection and response goes here.
 
-		int c = getc(stdin);
-
-		mvprintw(0, 0, "Terminal rows: %d\n", _rows);
-		mvprintw(1, 0, "Terminal cols: %d\n", _cols);
-
-		move(_rows / 2, _cols / 2); // Print to the center of the Screen.
-		printw("entered: %c", c);
+		// Update Entities with new positions and update animations to be drawn at the next iteration goes here.
+		wclear(stdscr);
 	}
 
 	endwin();
