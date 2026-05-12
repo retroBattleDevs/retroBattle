3c9dcd92d8f529f54dc07dec2a44653305baa231
NilouKS <nshahri21@hotmail.com>
2026-05-12 07:51:18 +0200

    Item Basisklasse verbessert

--- headers/Item.h
+++ headers/Item.h
@@ -1,44 +1,31 @@
 #pragma once
 #include <iostream>
-#include <string>
-#include "external_libraries/PDCurses/curses.h"
-
-enum class ItemType {
-	Equipment,
-	Consumable,
-	Relict
-
-};
+#include "Vec2d.h"
+#include "Player.h"
 
 class Item {
-
 protected:
-	int id;
-	ItemType type;
-
-	std::string name;
-	std::string description;
-	char* asciiSymbol;
+    Vec2d position;
+    char symbol;
 
 public:
-	Item(int id, ItemType type, const std::string& name, const std::string& description, const char* asciiSymbol);
+ 
+    Item();
+    Item(const Vec2d& pos, char sym);
 
-	// Rule of 5
-	virtual ~Item();
-	Item(const Item& other);
-	Item& operator=(const Item& other);
-	Item(Item&& other) noexcept;
-	Item& operator=(Item&& other) noexcept;
+    //Rule of Five
+    Item(const Item& other);                 
+    Item& operator=(const Item& other);      
+    Item(Item&& other) noexcept;             
+    Item& operator=(Item&& other) noexcept;  
+    virtual ~Item();                         
 
-	
-	int getId() const;
-	ItemType getType() const;
-	const std::string& getName() const;
-	const std::string& getDescription() const;
-	const char* getAsciiSymbol() const;
+  
+    Vec2d getPosition() const;
+    char getSymbol() const;
 
-	virtual void use() = 0; // Verhalten, wenn Spieler das Item benutzt 
-	virtual void inspect() const = 0; // Infoanzeige des Items, wenn Spieler das Item anschaut s
-
-
+    //Verhalten 
+    virtual void onPickup(Player& player) = 0;
 };
+
+
